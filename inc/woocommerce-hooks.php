6d2b507b3b21fbfc0befd8a9dfb981ae6e7dcc94
Natalia Lysak <linata81@gmail.com>
2023-03-04 17:38:09 +0300

    lesson 9

--- inc/woocommerce-hooks.php
+++ inc/woocommerce-hooks.php
@@ -1,3 +1,12 @@
 <?php
-/* сбросили стили woocommerce */
+/* отключили стили woocommerce */
 add_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
+
+/* список категорий товаров */
+remove_action('woocommerce_shop_loop_subcategory_title', 'woocommerce_template_loop_category_title', 10);
+
+add_action('woocommerce_shop_loop_subcategory_title', function($category) {
+  // woostudy_debug($category);
+  echo '<h6>'. esc_html($category->name).'</h6>';  /* выводим название категории */
+  echo '<small class="text-body">'.$category->count. __(' Products', 'woostudy').'</small>'; 
+});
--- woocommerce/content-product-cat.php
+++ woocommerce/content-product-cat.php
@@ -21,7 +21,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 <!-- дополнительный набор классов мы можем передать 1 аргументом -->
 <!-- а li заменим на div -->
-<div <?php wc_product_cat_class( 'col-lg-3 col-md-4 col-sm-6 pb-1', $category ); ?>>
+<div <?php wc_product_cat_class( 'col-lg-3 col-md-4 col-sm-6 pb-1 category-list', $category ); ?>>
 	<?php
 	/**
 	 * The woocommerce_before_subcategory hook.
@@ -29,26 +29,37 @@ if ( ! defined( 'ABSPATH' ) ) {
 	 * @hooked woocommerce_template_loop_category_link_open - 10
 	 */
 	do_action( 'woocommerce_before_subcategory', $category );
-
-	/**
-	 * The woocommerce_before_subcategory_title hook.
-	 *
-	 * @hooked woocommerce_subcategory_thumbnail - 10
-	 */
-	do_action( 'woocommerce_before_subcategory_title', $category );
-
+	?>
+	
+	<div class="cat-item d-flex align-items-center mb-4">	
+		<div class="overflow-hidden" style="width: 100px; height: 100px;">
+			<?php
+			/**
+			 * The woocommerce_before_subcategory_title hook.
+			 *
+			 * @hooked woocommerce_subcategory_thumbnail - 10
+			 */
+			do_action( 'woocommerce_before_subcategory_title', $category );
+			?>
+		</div> <!-- ./overflow-hidden -->
+		<div class="flex-fill pl-3">
+	<?php
 	/**
 	 * The woocommerce_shop_loop_subcategory_title hook.
 	 *
 	 * @hooked woocommerce_template_loop_category_title - 10
 	 */
 	do_action( 'woocommerce_shop_loop_subcategory_title', $category );
-
+	?>
+	</div> <!-- ./flex-fill pl-3 -->	
+	<?php
 	/**
 	 * The woocommerce_after_subcategory_title hook.
 	 */
 	do_action( 'woocommerce_after_subcategory_title', $category );
-
+	?>
+	</div> <!-- ./cat-item d-flex align-items-center mb-4 -->
+	<?php
 	/**
 	 * The woocommerce_after_subcategory hook.
 	 *
