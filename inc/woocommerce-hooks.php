d83635a17cae567bd784d65ad4bda243fc856e6f
Natalia Lysak <linata81@gmail.com>
2023-03-06 21:16:47 +0300

    lesson 11

--- inc/woocommerce-hooks.php
+++ inc/woocommerce-hooks.php
@@ -10,3 +10,21 @@ add_action('woocommerce_shop_loop_subcategory_title', function($category) {
   echo '<h6>'. esc_html($category->name).'</h6>';  /* выводим название категории */
   echo '<small class="text-body">'.$category->count. __(' Products', 'woostudy').'</small>'; 
 });
+
+// Ссылка на товар в карточке товара
+remove_action('woocommerce_before_shop_loop_item', 'woocommerce_template_loop_product_link_open', 10);
+remove_action('woocommerce_after_shop_loop_item', 'woocommerce_template_loop_product_link_close', 5);
+
+
+// Удаляем название товара woo в карточке и добавляем свой
+remove_action('woocommerce_shop_loop_item_title', 'woocommerce_template_loop_product_title', 10);
+add_action('woocommerce_shop_loop_item_title', function() {
+  // получим данные из глобального объекта 
+  //https://woocommerce.github.io/code-reference/classes/WC-Product.html
+  global $product;
+  /** @var WC_Product $product */
+  echo '<a class="h6 text-decoration-none text-truncate" href="'. $product->get_permalink() .'">'. $product->get_title() .'</a>';
+});
+
+// Рейтинг товара в карточке товара
+remove_action('woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_rating', 5);
