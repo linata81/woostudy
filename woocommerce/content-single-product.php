ebaa03079e56b164b7374d65be049938a95f6f26
Natalia Lysak <linata81@gmail.com>
2023-03-16 12:30:03 +0300

    lesson 20 single slider not variation product

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -1,44 +1,94 @@
 <?php
-/**
- * The template for displaying product content in the single-product.php template
- *
- * This template can be overridden by copying it to yourtheme/woocommerce/content-single-product.php.
- *
- * HOWEVER, on occasion WooCommerce will need to update template files and you
- * (the theme developer) will need to copy the new files to your theme to
- * maintain compatibility. We try to do this as little as possible, but it does
- * happen. When this occurs the version of the template file will be bumped and
- * the readme will list any important changes.
- *
- * @see     https://docs.woocommerce.com/document/template-structure/
- * @package WooCommerce\Templates
- * @version 3.6.0
- */
-
 defined( 'ABSPATH' ) || exit;
 
+// объект продукта, который используется для вывода инфы
 global $product;
-
-/**
- * Hook: woocommerce_before_single_product.
- *
- * @hooked woocommerce_output_all_notices - 10
- */
-do_action( 'woocommerce_before_single_product' );
-
-if ( post_password_required() ) {
-	echo get_the_password_form(); // WPCS: XSS ok.
-	return;
-}
 ?>
-<div id="product-<?php the_ID(); ?>" <?php wc_product_class( '', $product ); ?>>
+
+<div class="col-12">
+	<?php
+	/**
+	 * Hook: woocommerce_before_single_product.
+	 *
+	 * @hooked woocommerce_output_all_notices - 10
+	 */
+	do_action( 'woocommerce_before_single_product' );
+	?>
+</div>
+
+<!-- вывод самой информации о продукте -->
+<div id="product-<?php the_ID(); ?>" <?php wc_product_class( 'col-12 product-card', $product ); ?>>
+
+	<div class="row">
+		<div class="col-lg-5 mb-30">
+			<!-- slider -->
+			<!-- возможный вариант, но тут картинка не меняется у вариативных товаров -->
+			<div id="product-carousel" class="carousel slide" data-ride="carousel">
+					<div class="carousel-inner bg-light">
+						
+						<?php
+							$product_img_id = $product->get_image_id();
+							if($product_img_id) {
+								$main_img = wp_get_attachment_image_url($product_img_id, 'full');
+							} else {
+								$main_img = wc_placeholder_img_src('full');
+							}
+							$product_img_ids = $product->get_gallery_image_ids();
+						?>
+						<div class="carousel-item active">
+								<img class="w-100 h-100" src="<?php echo $main_img ?>" alt="Image">
+						</div>
+						
+						<?php if($product_img_ids): ?>
+							<?php foreach($product_img_ids as $product_img_id): ?>
+								<div class="carousel-item">
+									<img class="w-100 h-100" src="<?php echo wp_get_attachment_image_url($product_img_id, 'full'); ?>" alt="Image">
+								</div>
+							<?php endforeach; ?>
+						<?php endif; ?>
+							
+					</div>
+					
+					<?php if($product_img_ids): ?>
+						<a class="carousel-control-prev" href="#product-carousel" data-slide="prev">
+								<i class="fa fa-2x fa-angle-left text-dark"></i>
+						</a>
+						<a class="carousel-control-next" href="#product-carousel" data-slide="next">
+								<i class="fa fa-2x fa-angle-right text-dark"></i>
+						</a>
+					<?php endif; ?>
+			</div>
+		</div>
+		
+		<div class="col-lg-7 h-auto mb-30">
+			<!-- content -->
+		</div>
+	</div>
+	
+</div>
+
+
+
+
+
+
+
+
+
+
+<?php
+return;
+
+
+
+?>
 
 	<?php
 	/**
 	 * Hook: woocommerce_before_single_product_summary.
 	 *
-	 * @hooked woocommerce_show_product_sale_flash - 10
-	 * @hooked woocommerce_show_product_images - 20
+	 * @hooked woocommerce_show_product_sale_flash - 10 (является ли товар распродажей)
+	 * @hooked woocommerce_show_product_images - 20 (выводит галерею)
 	 */
 	do_action( 'woocommerce_before_single_product_summary' );
 	?>
@@ -71,6 +121,6 @@ if ( post_password_required() ) {
 	 */
 	do_action( 'woocommerce_after_single_product_summary' );
 	?>
-</div>
+
 
 <?php do_action( 'woocommerce_after_single_product' ); ?>
